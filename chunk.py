edcffb5d33419b9bd94ac861f857362751ad4b5d
Ali <13789384+Phalanthus@users.noreply.github.com>
2021-05-01 23:52:44 +0430

    Add files via upload

---
+++ chunk.py
@@ -0,0 +1,40 @@
+import pyperclip
+
+
+class Chunk:
+
+    chunks : []
+
+    def __len__(self):
+        return len(self.chunks)
+
+    def __getitem__(self, index):
+        return self.chunks[index]
+
+    def __init__(self, text):
+        sp = text.split(" ")
+
+        chunks = []
+        i = 0
+        chunk = ""
+        for wd in sp:
+
+            if len(wd) + len(chunk) + 6 <= 280:
+                if chunk != "":
+                    chunk = chunk + " " + wd
+                else:
+                    chunk = f"/{i}\n" + chunk + wd
+            else:
+                chunks = chunks + [chunk + "/ \n"]
+                i += 1
+                chunk = f"/{i}\n" + wd
+
+        if chunks == []:
+            chunks = chunks +  [chunk]
+        self.chunks = chunks
+
+    def get_chunk(self, i):
+        return str(self.chunks[i])
+
+
+
---
+++ readme.md
@@ -0,0 +1,21 @@
+How to run the program:
+	1) install pyperclip
+		pip3 install pyperclip
+	2) Write the whole text you want to send, in a file named "file.txt",
+		be sure to put it in the same directory.
+	
+	3) python main.py
+	
+	press ENTER and the tweets will be copied to your clipboard.
+	Do it until the program ends.
+
+	
+	ISSUES:
+	
+	If you encountered with unicodedecodeerror, it might be because of the enviornment you
+	are running the program with.
+	
+	For example the program ran successfuly in Bash(Windows), but failed in POWERSHELL.
+	I might try to fix that problem to make the code more portable later.
+
+	
